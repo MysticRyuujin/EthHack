80f68c3709d577d5d85e89c80f141addfc60b378
Tolga <tolgatasci1@gmail.com>
2018-02-04 19:15:11 +0300

    Update EthHack.py

--- EthHack.py
+++ EthHack.py
@@ -8,7 +8,8 @@ import os
 import random
 from ecdsa import SigningKey, SECP256k1
 from web3 import Web3, KeepAliveRPCProvider
-
+# get api key here https://etherscan.io
+explorer_api_key = ''
 # Configure SQL connection parameters
 # https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server
 # {ODBC Driver 13 for SQL Server}
@@ -32,8 +33,11 @@ def sql_upload(private,address,balance):
         with open(address, 'w') as f:
             f.write("PRIVATE: {0}\nADDRESS: {1}\nBALANCE: {2}\n".format(private, address, balance))
         f.close()
-
-
+# Orginal Balance Check 
+def checker(adress):
+	r = requests.get('https://api.etherscan.io/api?module=account&action=balance&address='+adress+'&tag=latest&apikey='+explorer_api_key)
+	data = r.json()["result"]
+	return data
 def ethhack():
     # My ghetto load balancing
     nodes = [Web3(KeepAliveRPCProvider(host='geth', port='8545')),
@@ -51,9 +55,9 @@ def ethhack():
         except:
             try:
                 web3 = nodes[int(not node)]
-                balance = web3.eth.getBalance(address)
+                balance = checker(address)
             except:
-                balance = 1000000000000000000
+                balance = 0
         if balance > 0:
             balance = balance / 1000000000000000000
             sql_upload(private.to_string().hex(),address,balance)
